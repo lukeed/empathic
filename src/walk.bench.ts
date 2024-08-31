11a4280fb03ad4c2e9f318ecd543b05f98b3096a
Luke Edwards <luke.edwards05@gmail.com>
2024-08-30 21:24:45 -0700

    chore: walk.up yield trees

--- src/walk.bench.ts
+++ src/walk.bench.ts
@@ -1,22 +1,48 @@
 import * as walk from './walk.ts';
 
 let start = '.';
-
-let totalA = 0;
-let totalB = 0;
-
 let exclude = [/\.tsx$/];
 
-Deno.bench('callback', async (b) => {
-	await walk.escalade(start, (d, arr) => {
-		totalB += arr.length;
-		// for (let x of arr) callback.add(x.name);
-	}, exclude);
+Deno.bench({
+	group: 'raw',
+	name: 'escalade',
+	async fn() {
+		let total = 0;
+		await walk.escalade(start, (_, items) => {
+			total += items.length;
+		});
+	},
 });
 
-Deno.bench('async gen', async (b) => {
-	for await (let tree of walk.up(start, { exclude })) {
-		// totalA += tree.items.length; // items.length;
-		totalA += 1;
-	}
+Deno.bench({
+	group: 'raw',
+	name: 'walk.up',
+	async fn() {
+		let total = 0;
+		for await (let tree of walk.up(start)) {
+			total += tree.items.length;
+		}
+	},
+});
+
+Deno.bench({
+	group: 'filter',
+	name: 'escalade',
+	async fn() {
+		let total = 0;
+		await walk.escalade(start, (_, items) => {
+			total += items.length;
+		}, exclude);
+	},
+});
+
+Deno.bench({
+	group: 'filter',
+	name: 'walk.up',
+	async fn() {
+		let total = 0;
+		for await (let tree of walk.up(start, { exclude })) {
+			total += tree.items.length;
+		}
+	},
 });
--- src/walk.ts
+++ src/walk.ts
@@ -3,13 +3,11 @@ import * as fs from 'node:fs/promises';
 
 import type { Dirent } from 'node:fs';
 
-type Entry = {
+type Tree = {
 	name: string;
 	parent: string;
-	isFile: boolean;
-	isDirectory: boolean;
-	isSymlink: boolean;
-	// isSocket: boolean;
+	path: string;
+	items: Dirent[];
 };
 
 type Options = {
@@ -63,31 +61,18 @@ function isMatch(arr: RegExp[], name: string): boolean {
 	return false;
 }
 
-type Tree = {
-	name: string;
-	parent: string;
-	path: string;
-	items: Dirent[];
-};
-
 export async function* up(
 	base: string,
 	options?: Options,
-	// ): AsyncGenerator<Tree> {
-): AsyncGenerator<Entry> {
-	let { cwd, limit, symlinks, exclude = [] } = options || {};
+): AsyncGenerator<Tree> {
+	let { cwd, limit, exclude = [] } = options || {};
 
 	cwd = resolve(cwd || '.');
 	limit = resolve(cwd, limit || '/');
 
-	let i = 0, len = 0;
+	let i = 0, len = 0, arr: Dirent[], tree: Tree;
 	let filter = exclude.length > 0;
-	// let j = 0, jlen = exclude.length;
-	// let name: string, filter = jlen > 0;
-
 	let dir = resolve(cwd, base);
-	let tree: Tree, arr: Dirent[];
-	let tmp: Dirent;
 
 	let s = await fs.stat(dir);
 	if (s.isFile()) dir = dirname(dir);
@@ -97,49 +82,31 @@ export async function* up(
 			withFileTypes: true, // v10.11
 		});
 
-		// tree = {
-		// 	name: basename(dir), // todo
-		// 	parent: dirname(dir),
-		// 	path: dir,
-		// 	items: arr,
-		// };
+		tree = {
+			name: basename(dir), // todo
+			parent: dirname(dir),
+			path: dir,
+			items: arr,
+		};
 
-		// if (filter) {
-		// 	tree.items = [];
-		// 	for (i = 0, len = arr.length; i < len; i++) {
-		// 		isMatch(exclude, arr[i].name) || tree.items.push(arr[i]);
-		// 	}
-		// 	// outer: for (i = 0, len = arr.length; i < len; i++) {
-		// 	// 	name = arr[i].name;
-		// 	// 	for (j = 0; j < jlen; j++) {
-		// 	// 		if (exclude[j].test(name)) continue outer;
-		// 	// 	}
-		// 	// 	tree.items.push(arr[i]);
-		// 	// }
-		// }
-
-		// yield tree;
-
-		// if (dir === limit) return;
-		// dir = tree.parent;
-
-		for (tmp of arr) {
-			if (filter && isMatch(exclude, tmp.name)) {
-				continue;
+		if (filter) {
+			tree.items = [];
+			for (i = 0, len = arr.length; i < len; i++) {
+				isMatch(exclude, arr[i].name) || tree.items.push(arr[i]);
 			}
-
-			yield {
-				name: tmp.name,
-				parent: dir,
-				isFile: tmp.isFile(),
-				isDirectory: tmp.isDirectory(),
-				isSymlink: tmp.isSymbolicLink(),
-				// isSocket: tmp.isSocket(),
-			};
+			// outer: for (i = 0, len = arr.length; i < len; i++) {
+			// 	name = arr[i].name;
+			// 	for (j = 0; j < jlen; j++) {
+			// 		if (exclude[j].test(name)) continue outer;
+			// 	}
+			// 	tree.items.push(arr[i]);
+			// }
 		}
+
+		yield tree;
 
 		// if this WAS the limit
 		if (dir === limit) return;
-		dir = dirname(dir);
+		dir = tree.parent;
 	}
 }
