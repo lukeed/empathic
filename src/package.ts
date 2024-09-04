60fc1c1651ec397d556d4bb0dac69de65711d252
aquapi <aquaplmc@gmail.com>
2024-09-04 12:29:27 +0700

    Package: Do explicit checks

--- src/package.ts
+++ src/package.ts
@@ -3,14 +3,14 @@ import { dirname, join } from 'node:path';
 import { existsSync, mkdirSync } from 'node:fs';
 
 import { writable } from 'empathic/access';
-import * as find from 'empathic/find';
+import { up as findUp, type Options as FindOptions } from 'empathic/find';
 
 /**
  * Find the closest "package.json" file while walking parent directories.
  * @returns The absolute path to a "package.json", if found.
  */
-export function up(options?: find.Options): string | undefined {
-	return find.up('package.json', options);
+export function up(options?: FindOptions): string | undefined {
+	return findUp('package.json', options);
 }
 
 /**
@@ -33,32 +33,29 @@ export function up(options?: find.Options): string | undefined {
  */
 export function cache(
 	name: string,
-	options?: find.Options & { create?: boolean },
+	options?: FindOptions & { create?: boolean },
 ): string | undefined {
-	options ||= {};
-
 	let dir = env.CACHE_DIR;
 
-	if (!dir || /^(1|0|true|false)$/.test(dir)) {
-		let pkg = up(options);
+	if (typeof dir === 'undefined' || dir === '1' || dir === '0' || dir === 'true' || dir === 'false') {
+		const pkg = up(options);
 
 		if (dir = pkg && dirname(pkg)) {
-			let mods = join(dir, 'node_modules');
-			let exists = existsSync(mods);
+			const mods = join(dir, 'node_modules');
 
 			// exit cuz exists but not writable
 			// or cuz missing but parent not writable
-			if (!writable(exists ? mods : dir)) return;
+			if (!writable(existsSync(mods) ? mods : dir)) return;
 
 			dir = join(mods, '.cache');
 		}
 	}
 
-	if (dir) {
-		dir = join(dir, name);
-		if (options.create && !existsSync(dir)) {
-			mkdirSync(dir, { recursive: true });
-		}
-		return dir;
+	if (typeof dir === 'string') {
+		const targetDir = join(dir, name);
+		if (options?.create === true && !existsSync(targetDir))
+			mkdirSync(targetDir, { recursive: true });
+
+		return targetDir;
 	}
 }
